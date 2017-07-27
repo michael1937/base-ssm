c89e7cb1aeb0c70643bde3f0993f0e5391ef4ac7
zengdongling <zengdongling@kanglefu.com>
2017-07-27 17:52:42 +0800

    代码生成

--- src/main/java/com/kanglefu/modules/demo/controller/UserController.java
+++ src/main/java/com/kanglefu/modules/demo/controller/UserController.java
@@ -1,14 +1,11 @@
 package com.kanglefu.modules.demo.controller;
 
-import com.eryansky.core.security.annotation.RequiresUser;
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
 import com.kanglefu.modules.base.controller.BaseController;
 import com.kanglefu.modules.base.controller.result.Result;
-import com.kanglefu.modules.demo.model.CommonTest;
 import com.kanglefu.modules.demo.model.User;
 import com.kanglefu.modules.demo.model.vo.UserVo;
-import com.kanglefu.modules.demo.service.ICommonTestService;
 import com.kanglefu.modules.demo.service.IUserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,7 +13,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Date;
 import java.util.List;
 
 /**
@@ -29,19 +25,6 @@ public class UserController extends BaseController {
     @Autowired
     private IUserService userService;
 
-    @Autowired
-    private ICommonTestService commonTestService;
-
-    @RequiresUser(required = false)
-    @RequestMapping("/common/save")
-    public Result commonSave(CommonTest test){
-        test.setCreateTime(new Date());
-        commonTestService.save(test);
-
-        return Result.success().setData(test);
-    }
-
-    @RequiresUser(required = false)
     @RequestMapping("/test/{name}")
     public Result test(@PathVariable("name") String name){
         User user = new User(name);
@@ -53,7 +36,6 @@ public class UserController extends BaseController {
     /**
      *  分页测试1
      */
-    @RequiresUser(required = false)
     @RequestMapping("getUserPage")
     public Result getUserPage(User user, @RequestParam int pageNo, @RequestParam int pageSize) {
 
@@ -67,7 +49,6 @@ public class UserController extends BaseController {
     /**
      *  分页测试2
      */
-    @RequiresUser(required = false)
     @RequestMapping("getUserVoPage")
     public Result getUserVoPage(User user, @RequestParam int pageNo, @RequestParam int pageSize) {
 
